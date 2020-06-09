44f682c371d32e6500196e0071d29c854c43e1b7
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-06-09 09:40:12 +0530

    Update simple_calculator.py

--- tkinter/calculator/simple_calculator.py
+++ tkinter/calculator/simple_calculator.py
@@ -13,7 +13,7 @@ def enter(x):
     pass
 
 # Create a screen
-scr_top = Entry(root, width=50, borderwidth=10, bg="#dedede", fg="#004f3b")
+scr_top = Entry(root, width=32, borderwidth=10, bg="#dedede", fg="#004f3b")
 scr_top.grid(row = 0, columnspan = 4)
 
 def btn_rec(num_sym):
@@ -62,52 +62,52 @@ def processing():
 
 # create button 7
 
-btn_seven = Button(root, text = "7", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(7))
+btn_seven = Button(root, text = "7", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(7))
 # add the button_seven to the grid.
 btn_seven.grid(row = 1, column = 0)
 
 # Create buttons 6 to 0
 
-btn_eight = Button(root, text = "8", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(8))
+btn_eight = Button(root, text = "8", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(8))
 btn_eight.grid(row = 1, column = 1)
 
-btn_nine = Button(root, text = "9", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(9))
+btn_nine = Button(root, text = "9", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(9))
 btn_nine.grid(row = 1, column = 2)
 
-btn_four = Button(root, text = "4", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(4))
+btn_four = Button(root, text = "4", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(4))
 btn_four.grid(row = 2, column = 0)
 
-btn_five = Button(root, text = "5", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(5))
+btn_five = Button(root, text = "5", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(5))
 btn_five.grid(row = 2, column = 1)
 
-btn_six = Button(root, text = "6", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(6))
+btn_six = Button(root, text = "6", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(6))
 btn_six.grid(row = 2, column = 2)
 
-btn_one = Button(root, text = "1", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(1))
+btn_one = Button(root, text = "1", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(1))
 btn_one.grid(row = 3, column = 0)
 
-btn_two = Button(root, text = "2", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(2))
+btn_two = Button(root, text = "2", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(2))
 btn_two.grid(row = 3, column = 1)
 
-btn_three = Button(root, text = "3", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(3))
+btn_three = Button(root, text = "3", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(3))
 btn_three.grid(row = 3, column = 2)
 
-btn_three = Button(root, text = "0", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(0))
+btn_three = Button(root, text = "0", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec(0))
 btn_three.grid(row = 4, column = 0)
 
-btn_enter = Button(root, text = "Enter", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = output_screen)
+btn_enter = Button(root, text = "Enter", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = output_screen)
 btn_enter.grid(row = 4, column = 1)
 
-btn_add = Button(root, text = "+", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec("+"))
+btn_add = Button(root, text = "+", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec("+"))
 btn_add.grid(row = 1, column = 3)
 
-btn_sub = Button(root, text = "-", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec("-"))
+btn_sub = Button(root, text = "-", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec("-"))
 btn_sub.grid(row = 2, column = 3)
 
-btn_prod = Button(root, text = "X", state="disabled", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec("*"))
+btn_prod = Button(root, text = "X", state="disabled", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = lambda: btn_rec("*"))
 btn_prod.grid(row = 3, column = 3)
 
-btn_prod = Button(root, text = "Clear", bd = 5, width = 10, height = 5, padx = 3, bg = "#b0cfc7", command = clear_screen)
+btn_prod = Button(root, text = "Clear", bd = 5, width = 5, height = 3, padx = 3, bg = "#b0cfc7", command = clear_screen)
 btn_prod.grid(row = 4, column = 2)
 
 
